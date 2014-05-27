9ec78b7065238356335e4f6acf21248a9ad5e70b
Dmitriy Brodnitskiy <dzmitry_bradnitski@epam.com>
2014-05-27 18:48:51 +0300

    asd

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module NinjaDashboard
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
 
-    config.autoload_paths += ["#{Rails.root}/lib/vsphere/lib/vsphere.rb"]
+    config.autoload_paths += ["#{config.root}/lib"]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
