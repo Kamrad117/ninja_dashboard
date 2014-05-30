cd213df39d37d87f7249d9ed5aeefcb8d4789b2d
Dmitriy Brodnitskiy <dzmitry_bradnitski@epam.com>
2014-05-30 14:47:33 +0300

    add loading indicator

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,10 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+
+$(document).on('page:fetch', function() {
+  $('#page_loading').show();
+});
+$(document).on('page:change', function() {
+  $('#page_loading').hide();
+});
